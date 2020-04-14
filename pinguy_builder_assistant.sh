c78101e84f8a8624e5bdc696ed206af4fa11d687
nexryai <61890205+nexryai@users.noreply.github.com>
2020-04-14 18:08:31 +0900

    Add files via upload

---
+++ pinguy_builder_assistant.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo ========================
+echo pinguy builder assistant
+echo ========================
+echo 設定ファイルのコピーを開始します。root権限を使用します。
+sudo cp -r $HOME/.local/share/applications/ /etc/skel/.local/
+sudo cp -r $HOME/.config/autostart/ /etc/skel/.config/
+sudo cp -r $HOME/.config/dconf/ /etc/skel/.config/
+sudo cp -r $HOME/.config/gtk-2.0/ /etc/skel/.config/
+sudo cp -r $HOME/.config/gtk-3.0/ /etc/skel/.config/
+sudo cp -r $HOME/.config/Thunar/ /etc/skel/.config/
+sudo cp -r $HOME/.config/xfce4/ /etc/skel/.config/
+echo システムをアップデートします
+sudo apt-get update && apt-get upgrade -y
+echo 不要な依存関係を削除します
+sudo apt autoremove
+echo 準備は完了しました。PinguyBuilderを使用してisoをビルドしてください。
+sudo PinguyBuilder-gtk
